fbd83770aa3b3809861a0c8ed9c0d4a64c38856a
taypedev <jetaypedev@gmail.com>
2026-04-15 16:12:28 -0500

    feat: implementar logger con formato y color para diferentes niveles de log

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -7,3 +7,24 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+type TLanguage = "en" | "es" | "fr";
+
+function createGreeter(language: TLanguage) {
+  return function (name: string) {
+    const messages: Record<TLanguage, string> = {
+      es: `¡Hola, ${name}!`,
+      en: `Hello, ${name}!`,
+      fr: `Bonjour, ${name}!`,
+    };
+
+    return console.log(messages[language]);
+  };
+}
+
+function main() {
+  const spanishGreeter = createGreeter("es");
+  spanishGreeter("Juan");
+}
+
+main();
