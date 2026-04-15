9a0605bddd27bdf390c11c89dee90ca21078d2db
taypedev <jetaypedev@gmail.com>
2026-04-14 22:19:05 -0500

    feat: implementar clase DocumentC con métodos para clonar y mostrar información

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,55 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class DocumentC {
+  title: string;
+  private content: string;
+  author: string;
+
+  constructor(title: string, content: string, author: string) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+
+  clone(): DocumentC {
+    return new DocumentC(this.title, this.content, this.author);
+  }
+
+  displayInfo() {
+    console.log(`
+      Título: ${this.title}
+      Contenido: ${this.content}
+      Autor: ${this.author}
+    `);
+  }
+}
+
+function main() {
+  const originalDoc = new DocumentC(
+    "Documento Original",
+    "Este es el contenido del documento original.",
+    "Juan Pérez",
+  );
+  const clonedDoc = originalDoc.clone();
+
+  console.log("Documento Original:");
+  originalDoc.displayInfo();
+
+  console.log("Documento Clonado:");
+  clonedDoc.displayInfo();
+
+  // Modificando el documento clonado para demostrar que son independientes
+  clonedDoc.title = "Documento Clonado Modificado";
+  clonedDoc.author = "María Gómez";
+
+  console.log("Después de modificar el documento clonado:");
+  console.log("Documento Original:");
+  originalDoc.displayInfo();
+
+  console.log("Documento Clonado:");
+  clonedDoc.displayInfo();
+}
+
+main();
