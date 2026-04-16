71b4cec9239cc813009ec7047e55a86d372645f0
taypedev <jetaypedev@gmail.com>
2026-04-15 20:16:47 -0500

    feat: implementar patrón Composite con clases File y Folder para representar estructuras jerárquicas

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,60 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+  showDetails(indent?: string): void;
+}
+
+class File implements FileSystemComponent {
+  constructor(private name: string) {}
+
+  showDetails(indent: string = "") {
+    console.log(`${indent}+ Archivo: ${this.name}`);
+  }
+}
+
+class Folder implements FileSystemComponent {
+  private children: FileSystemComponent[] = [];
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  showDetails(indent?: string): void {
+    console.log(`${indent}- Carpeta: ${this.name}`);
+    this.children.forEach((child) => child.showDetails(indent + "  "));
+  }
+
+  add(component: FileSystemComponent) {
+    this.children.push(component);
+  }
+
+  remove(component: FileSystemComponent) {
+    const index = this.children.indexOf(component);
+    if (index !== -1) {
+      this.children.splice(index, 1);
+    }
+  }
+}
+
+// Uso del patrón Composite
+function main() {
+  const root = new Folder("root");
+  const folder1 = new Folder("folder1");
+  const folder2 = new Folder("folder2");
+  const file1 = new File("file1.txt");
+  const file2 = new File("file2.txt");
+  const file3 = new File("file3.txt");
+
+  folder1.add(file1);
+  folder1.add(file2);
+  folder2.add(file3);
+  root.add(folder1);
+  root.add(folder2);
+
+  root.showDetails("");
+}
+
+main();
